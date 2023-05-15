531dc1ca35161f3e06b362ec13c5823374aaaa97
Bertrand <bigoulours@hotmail.com>
2023-05-15 23:39:04 +0200

    adding missing methods to rooms api

--- slskd_api/apis/rooms.py
+++ slskd_api/apis/rooms.py
@@ -2,7 +2,7 @@ from .base import *
 
 class RoomsApi(BaseApi):
 
-    def get_joined(self):
+    def get_all_joined(self):
         """
         Gets all joined rooms.
         """
@@ -20,8 +20,68 @@ class RoomsApi(BaseApi):
         return response.json()
 
 
-    # ToDo: add missing rooms methods
+    def get_joined(self, roomName):
+        """
+        Gets the specified room.
+        """
+        url = self.api_url + f'/rooms/joined/{roomName}'
+        response = requests.get(url, headers=self.header)
+        return response.json()
+    
 
+    def leave(self, roomName):
+        """
+        Leaves a room.
+        """
+        url = self.api_url + f'/rooms/joined/{roomName}'
+        response = requests.delete(url, headers=self.header)
+        return response.ok
+    
+
+    def send(self, roomName, message):
+        """
+        Sends a message to the specified room.
+        """
+        url = self.api_url + f'/rooms/joined/{roomName}/messages'
+        response = requests.post(url, headers=self.header, json=message)
+        return response.ok
+
+
+    def get_messages(self, roomName):
+        """
+        Gets the current list of messages for the specified room.
+        """
+        url = self.api_url + f'/rooms/joined/{roomName}/messages'
+        response = requests.get(url, headers=self.header)
+        return response.json()
+
+
+    def set_tocker(self, roomName, ticker):
+        """
+        Sets a ticker for the specified room.
+        """
+        url = self.api_url + f'/rooms/joined/{roomName}/ticker'
+        response = requests.post(url, headers=self.header, json=ticker)
+        return response.ok
+    
+
+    def add_member(self, roomName, username):
+        """
+        Adds a member to a private room.
+        """
+        url = self.api_url + f'/rooms/joined/{roomName}/members'
+        response = requests.post(url, headers=self.header, json=username)
+        return response.ok
+    
+
+    def get_users(self, roomName):
+        """
+        Gets the current list of users for the specified room.
+        """
+        url = self.api_url + f'/rooms/joined/{roomName}/users'
+        response = requests.get(url, headers=self.header)
+        return response.json()
+    
 
     def get_all(self):
         """
