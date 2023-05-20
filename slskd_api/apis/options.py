77fe341aeddcfc61b50542bd93fcceb17e42a7cf
bigoulours <bigoulours@hotmail.com>
2023-05-20 10:01:04 +0200

    adding missing options methods

--- slskd_api/apis/options.py
+++ slskd_api/apis/options.py
@@ -23,12 +23,56 @@ class OptionsApi(BaseApi):
         return response.json()
 
   
-# Getting error 'Unauthorized'(API-Key) or 'Forbidden' (token):
-    # def get_debug(self) -> dict:
-    #     """
-    #     Gets the debug view of the current application options.
-    #     """
-    #     url = self.api_url + '/options/debug'
-    #     response = requests.get(url, headers=self.header)
-    #     return response.json()
+    def debug(self) -> str:
+        """
+        Gets the debug view of the current application options.
+        debug and remote_configuration must be set to true.
+        Only works with token (usr/pwd login). 'Unauthorized' with API-Key.
+        """
+        url = self.api_url + '/options/debug'
+        response = requests.get(url, headers=self.header)
+        return response.json()
+
+
+    def yaml_location(self) -> str:
+        """
+        Gets the path of the yaml config file. remote_configuration must be set to true.
+        Only works with token (usr/pwd login). 'Unauthorized' with API-Key.
+        """
+        url = self.api_url + '/options/yaml/location'
+        response = requests.get(url, headers=self.header)
+        return response.json()
     
+
+    def download_yaml(self) -> str:
+        """
+        Gets the content of the yaml config file as text. remote_configuration must be set to true.
+        Only works with token (usr/pwd login). 'Unauthorized' with API-Key.
+        """
+        url = self.api_url + '/options/yaml'
+        response = requests.get(url, headers=self.header)
+        return response.json()
+    
+
+    def upload_yaml(self, yaml_content: str) -> bool:
+        """
+        Sets the content of the yaml config file. remote_configuration must be set to true.
+        Only works with token (usr/pwd login). 'Unauthorized' with API-Key.
+
+        :return: True if successful.
+        """
+        url = self.api_url + '/options/yaml'
+        response = requests.post(url, headers=self.header, json=yaml_content)
+        return response.ok
+    
+
+    def validate_yaml(self, yaml_content: str) -> str:
+        """
+        Validates the provided yaml string. remote_configuration must be set to true.
+        Only works with token (usr/pwd login). 'Unauthorized' with API-Key.
+
+        :return: Empty string if validation successful. Error message otherwise.
+        """
+        url = self.api_url + '/options/yaml/validate'
+        response = requests.post(url, headers=self.header, json=yaml_content)
+        return response.text
